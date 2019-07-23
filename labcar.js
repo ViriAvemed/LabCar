229320ac30f6ad4a1d19311d2a9390b997a05401
ViriAvemed <viridiana.avem@gmail.com>
2019-07-22 23:53:35 -0500

    Tercer modal

--- labcar.js
+++ labcar.js
@@ -1,5 +1,5 @@
 
-
+//FIRST MODAL
 // Get the modal Login
 const modal = document.getElementById("myModal");
 
@@ -26,6 +26,8 @@ window.onclick = function(event) {
     }
 };
 
+
+//SECOND MODAL
 //Get the modal Register
 const modal2 = document.getElementById("myModal2");
 
@@ -49,5 +51,32 @@ span2.onclick = function() {
 window.onclick = function(event) {
     if (event.target === modal2) {
         modal2.style.display = "none";
+    }
+};
+
+//3rd MODAL
+//Get the modal Register
+const modal3 = document.getElementById("myModal3");
+
+// Get the button that opens the modal
+const btn3 = document.getElementById("id-driver");
+
+// Get the <span> element that closes the modal
+const span3 = document.getElementsByClassName("close3")[0];
+
+// When the user clicks the button, open the modal
+btn3.onclick = function() {
+    modal3.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span3.onclick = function() {
+    modal3.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target === modal3) {
+        modal3.style.display = "none";
     }
 };
